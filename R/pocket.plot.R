83f40257b67084cd15813d4874dc75539f1b2b06
Alí Santacruz <amsantac@unal.edu.co>
2012-04-19 00:00:00 +0000

    version 0.5-0

--- R/pocket.plot.R
+++ R/pocket.plot.R
@@ -15,11 +15,12 @@
 
 
 assign("pocket.plot",
-   function(data, graph, X, Y, Z,...){
-Pocket.plotA<-function(datos,statistical,X,Y,Z, direct, xlab){
+   function(data, graph, X, Y, Z, Iden,...){
+if(!is.logical(Iden))  stop(paste("Iden must be logical class"))
+Pocket.plotA<-function(datos,statistical,X,Y,Z,Iden,direct,xlab){
 grid.mat<-tapply(Z,list(factor(Y),factor(X)),mean)
 grid.matt<-grid.mat[max(nrow(grid.mat)):1,]
-
+par(mfrow=c(1,1), mar=c(4,4.5,2.5,2.5))
 
 vect<-function(grid.matt,j){
     n<-length(grid.matt)
@@ -32,7 +33,7 @@ vect<-function(grid.matt,j){
 matriz<-vect(grid.matt,1:ncol(grid.matt))
 
 b<-matrix(matriz[1:nrow(grid.matt)-1,],nrow=nrow(grid.matt)-1)
-y.barra<-mean(b,na.rm=TRUE)
+y.barra<-mean(b,na.rm=T)
 
 
 poc<-function(grid.matt,i,j){
@@ -59,8 +60,8 @@ media.conteo<-function(f,x){
     mean.f<- matrix(rep(0,nrow(f[,,1])*nrow(f[,,1])),nrow(f[,,1]),nrow(f[,,1]))
     U<- matrix(rep(0,nrow(f[,,1])*nrow(f[,,1])),nrow(f[,,1]),nrow(f[,,1]))
     for(j in 1:nrow(f[,,1])){
-    mean.f[j,]<-apply(f[,,j],1,mean,na.rm=TRUE)
-    U[j,]<- apply(f[,,j],1,sum,na.rm=TRUE)
+    mean.f[j,]<-apply(f[,,j],1,mean,na.rm=T)
+    U[j,]<- apply(f[,,j],1,sum,na.rm=T)
     }
     switch(x,
     media.barra=mean.f,
@@ -79,34 +80,40 @@ colnames(tabla)<-rotulo
 rownames(tabla)<-rotulo
 tabla1<-tabla[max(nrow(tabla)):1,]
 N1<-as.vector(t(tablaN))
-tabla2<-data.frame(P=as.vector(t(tabla1)),group=rep(factor(colnames(tabla1),levels=colnames(tabla1)),nrow(tabla1)),group1=rep(factor(rownames(tabla1),levels=rownames(tabla1)),rep(ncol(tabla1),ncol(tabla1))),N1,Q=(N1^0.5)*((as.vector(t(tabla1+y.barra))/y.barra)-1))
-Prob<-function(P){
-boxplot(P~group1, data=tabla2, main = paste("POCKET-PLOT in", direct, "direction"), col.main="blue", col=4, cex=0.7, out=TRUE, ylab= "P(jk)", xlab=paste(xlab, "Number"), lwd=1.5, col.main="blue", col.lab="blue", cex.lab=1.2)
+tabla2<-na.omit(data.frame(P=as.vector(t(tabla1)),group=rep(factor(colnames(tabla1),levels=colnames(tabla1)),nrow(tabla1)),group1=rep(factor(rownames(tabla1),levels=rownames(tabla1)),rep(ncol(tabla1),ncol(tabla1))),N1,Q=(N1^0.5)*((as.vector(t(tabla1+y.barra))/y.barra)-1)))
+Prob<-function(P,Iden){
+ifelse(Iden==F,bp.with.outlier.label(tabla2$P~tabla2$group1, tabla2$group, data=tabla2, main = paste("POCKET-PLOT in", direct, "direction"), 
+col.main="blue", col=4, cex=0.4, out=T, ylab= expression(P[jk]), xlab=paste(xlab, "Number"), lwd=1.5, col.main="blue", col.lab="blue", cex.lab=1.2, 
+spread_text = T),boxplot(P~group1, data=tabla2, main = paste("POCKET-PLOT in", direct, "direction"), col.main="blue", col=4, cex=0.7, out=T, 
+ylab= expression(P[jk]), xlab=paste(xlab, "Number"), lwd=1.5, col.main="blue", col.lab="blue", cex.lab=1.2))
 box(lty = 'solid', col = 'blue', lwd=2)
 abline(h=0, col="green")
-identify(tabla2$group1, tabla2$P, tabla2$group, cex=0.8)
+ifelse(Iden==T,identify(tabla2$group1, tabla2$P, tabla2$group, cex=0.8),"")
 }
-                             #main=paste("Sigma graphic optimization", func)
-Var<-function(V){
-boxplot(Q~group1, data=tabla2, main = paste("POCKET-PLOT of Standardized Variance in", direct, "direction"), col.main="blue", col=4, cex=0.7, out=TRUE, ylab= "Q(jk)", xlab=paste(xlab, "Number"), lwd=1.5, col.main="blue", col.lab="blue", cex.lab=1.2)
+                            
+Var<-function(V,Iden){
+ifelse(Iden==F,bp.with.outlier.label(tabla2$Q~tabla2$group1, tabla2$group, data=tabla2, main = paste("POCKET-PLOT of Standardized Variance in", direct, 
+"direction"), col.main="blue", col=4, cex=0.7, out=T, ylab= expression(Q[jk]), xlab=paste(xlab, "Number"), lwd=1.5, col.main="blue", col.lab="blue", cex.lab=1.2, 
+spread_text = T),boxplot(Q~group1, data=tabla2, main = paste("POCKET-PLOT of Standardized Variance in", direct, "direction"), col.main="blue", col=4, cex=0.7, 
+out=T, ylab= expression(Q[jk]), xlab=paste(xlab, "Number"), lwd=1.5, col.main="blue", col.lab="blue", cex.lab=1.2))
 box(lty = 'solid', col = 'blue', lwd=2)
 abline(h=0, col="green")
-identify(tabla2$group1, tabla2$Q, tabla2$group, cex=0.8)
+ifelse(Iden==T,identify(tabla2$group1, tabla2$Q, tabla2$group, cex=0.8),"")
 }
 
 
 #print(tabla2)
 switch(statistical,
-P=Prob(tabla2),
-V=Var(tabla2)
+P=Prob(tabla2,Iden),
+V=Var(tabla2, Iden)
 )
 }
 
 switch(graph,
-PPR=Pocket.plotA(datos, "P", X, Y, Z, direc="south-north", xlab="Row"),
-PPC=Pocket.plotA(datos, "P", Y, X, Z, direc="east-west", xlab="Column"),
-PVR=Pocket.plotA(datos, "V", X, Y, Z, direc="south-north", xlab="Row"),
-PVC=Pocket.plotA(datos, "V", Y, X, Z, direc="east-west", xlab="Column"),
+PPR=Pocket.plotA(datos, "P", X, Y, Z, direc="south-north", xlab="Row", Iden),
+PPC=Pocket.plotA(datos, "P", Y, X, Z, direc="east-west", xlab="Column", Iden),
+PVR=Pocket.plotA(datos, "V", X, Y, Z, direc="south-north", xlab="Row", Iden),
+PVC=Pocket.plotA(datos, "V", Y, X, Z, direc="east-west", xlab="Column", Iden),
 )
 
 })
